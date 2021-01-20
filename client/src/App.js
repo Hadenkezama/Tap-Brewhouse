af9c4bc8d5e44128375bcb87382dda87bc983909
Haden Kezama <hadenkezama@Hadens-iMac.gateway.sasktel.net>
2021-01-20 12:38:52 -0600

    added files to create layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import MainContent from './components/MainContent'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Navbar />
+      <MainContent />
+      <Sidebar />
+      <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ client/src/components/PurchasePopUp.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const PurchasePopUp = () => {
+  return <div>PurchasePopUp</div>
+}
+
+export default PurchasePopUp
