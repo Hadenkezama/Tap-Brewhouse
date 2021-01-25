04621397ef61e2885c2f778adfed348a3d1aeba6
Haden Kezama <hadenkezama@Hadens-iMac.gateway.sasktel.net>
2021-01-25 09:12:55 -0600

    finished the catagorie filtering

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Sidebar from './components/Sidebar'
 import Footer from './components/Footer'
 import LiquorLoader from './API loaders/LiquorLoader'
 import CatagorieLiquorLoader from './API loaders/CatagorieLiquorLoader'
+import CatagorieWineLoader from './API loaders/CatagorieWineLoader'
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 const App = () => {
@@ -19,6 +21,21 @@ const App = () => {
             path="/:drinks/catagorie/:catagorie"
             exact
             component={CatagorieLiquorLoader}
+          />
+          <Route
+            path="/wines/catagorie/wine_colour/:wine_colour"
+            exact
+            component={CatagorieWineLoader}
+          />
+          <Route
+            path="/wines/catagorie/wine_region/:wine_region"
+            exact
+            component={CatagorieWineLoader}
+          />
+          <Route
+            path="/wines/catagorie/wine_colour/:wine_colour/wine_region/:wine_region"
+            exact
+            component={CatagorieWineLoader}
           />
         </Switch>
         <Sidebar />
